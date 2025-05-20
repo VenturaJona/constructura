e4450f58bb65118089d346522a25875ccd9f8f8b
VenturaJona <venturajona999@gmail.com>
2025-05-19 20:33:18 -0600

    Update conexion.py

--- conexion/conexion.py
+++ conexion/conexion.py
@@ -1,9 +1,8 @@
 import sqlite3
 
 def conexion():
-    try:
-        #conn = sqlite3.connect('C:/Users/Eduardo Aragon/Downloads/constructora.db')  # Ruta a la base de datos SQLite
-        conn = sqlite3.connect('C:/xampp/htdocs/constructora/constructora.db')
+    try: 
+        conn = sqlite3.connect('../constructora.db')
         return conn
     except sqlite3.Error as err:
         print(f"Error de conexión: {err}")
